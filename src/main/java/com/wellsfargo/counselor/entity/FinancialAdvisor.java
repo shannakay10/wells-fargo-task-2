ccceeca992b5ad0e50a844e2755007ddcdef8fd1
shannakay10 <143737955+shannakay10@users.noreply.github.com>
2023-11-29 20:21:13 -0500

    New

--- src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -5,30 +5,21 @@ import javax.persistence.Id;
 
 @Entity
 public class FinancialAdvisor {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
-    // Other instance variables or relationships related to FinancialAdvisor
-
     public FinancialAdvisor() {
-        // Default constructor for JPA
     }
 
-    public FinancialAdvisor(String name /* Other parameters */) {
+    public FinancialAdvisor(String name) {
         this.name = name;
-        // Initialize other instance variables
     }
 
-    // Getter and Setter methods
     public Long getId() {
         return id;
     }
-
-    // No setId() method to make id read-only
 
     public String getName() {
         return name;
@@ -37,36 +28,25 @@ public class FinancialAdvisor {
     public void setName(String name) {
         this.name = name;
     }
-
-    // Other Getter and Setter methods for additional instance variables or relationships
 }
 
 @Entity
 class Client {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
-    // Other instance variables or relationships related to Client
-
+    
     public Client() {
-        // Default constructor for JPA
     }
 
-    public Client(String name /* Other parameters */) {
+    public Client(String name) {
         this.name = name;
-        // Initialize other instance variables
     }
 
-    // Getter and Setter methods
     public Long getId() {
         return id;
     }
-
-    // No setId() method to make id read-only
 
     public String getName() {
         return name;
@@ -75,67 +55,47 @@ class Client {
     public void setName(String name) {
         this.name = name;
     }
-
-    // Other Getter and Setter methods for additional instance variables or relationships
 }
 
 @Entity
 class Portfolio {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Other instance variables or relationships related to Portfolio
-
     public Portfolio() {
-        // Default constructor for JPA
     }
 
-    // Getter and Setter methods
     public Long getId() {
         return id;
     }
-
-    // No setId() method to make id read-only
-
-    // Other Getter and Setter methods for additional instance variables or relationships
 }
 
 @Entity
 class Security {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String category;
     private String purchaseDate;
     private double purchasePrice;
     private int quantity;
 
-    // Other instance variables or relationships related to Security
-
     public Security() {
-        // Default constructor for JPA
     }
 
-    public Security(String name, String category, String purchaseDate, double purchasePrice, int quantity /* Other parameters */) {
+    public Security(String name, String category, String purchaseDate, double purchasePrice, int quantity) {
         this.name = name;
         this.category = category;
         this.purchaseDate = purchaseDate;
         this.purchasePrice = purchasePrice;
         this.quantity = quantity;
-        // Initialize other instance variables
     }
 
-    // Getter and Setter methods
     public Long getId() {
         return id;
     }
-
-    // No setId() method to make id read-only
 
     public String getName() {
         return name;
@@ -176,6 +136,4 @@ class Security {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
-    // Other Getter and Setter methods for additional instance variables or relationships
 }
